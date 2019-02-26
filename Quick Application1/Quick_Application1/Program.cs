0fe3a0a2a525554e0250ffc4a99b2f8e68a01f56
Jorge Tovar <jorgetov@herbalife.com>
2019-02-26 17:52:28 -0600

    Add project files.

---
+++ Quick Application1/Quick_Application1/Program.cs
@@ -0,0 +1,27 @@
+// ====================================================
+// More Templates: https://www.ebenmonney.com/templates
+// Email: support@ebenmonney.com
+// ====================================================
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Quick_Application1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
